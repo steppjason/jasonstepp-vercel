31913a99c1c4d6d174b1d2ed5ea8514b776fac39
Jason Stepp <hi@jasonstepp.me>
2023-08-26 14:31:18 -0700

    fix tailwind css

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: 'Projects',
+    description: 'Projects description',
+  }
+
+export default function Projects(){
+    return (
+        <div><h1>Projects</h1></div>
+    )
+}
