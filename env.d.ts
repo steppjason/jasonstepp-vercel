dd42f4d857d1941db3177323769930c27a045236
Jason Stepp <hi@jasonstepp.me>
2023-09-01 17:47:17 -0700

    Add other auths

--- env.d.ts
+++ env.d.ts
@@ -1,8 +1,14 @@
 namespace NodeJS {
 	interface ProcessEnv {
 		DATABASE_URL: string
-		GOOGLE_CLIENT: string
-		GOOGLE_SECRET: string
+		GOOGLE_CLIENT_ID: string
+		GOOGLE_CLIENT_SECRET: string
+		FACEBOOK_CLIENT_ID: string
+		FACEBOOK_CLIENT_SECRET: string
+		DISCORD_CLIENT_ID: string
+		DISCORD_CLIENT_SECRET: string
+		GITHUB_ID: string
+		GITHUB_SECRET: string
 		NEXTAUTH_URL: string
 		NEXTAUTH_SECRET: string
 	}
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,29 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
+// import FacebookProvider from 'next-auth/providers/facebook'
+// import DiscordProvider from 'next-auth/providers/discord'
+// import GitHubProvider from 'next-auth/providers/github'
 
 import type { NextAuthOptions } from 'next-auth'
 
 const authOptions: NextAuthOptions = {
 	providers: [
 		GoogleProvider({
-			clientId: process.env.GOOGLE_CLIENT,
-			clientSecret: process.env.GOOGLE_SECRET,
+			clientId: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 		}),
+		// FacebookProvider({
+		// 	clientId: process.env.FACEBOOK_CLIENT_ID,
+		// 	clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+		// }),
+		// DiscordProvider({
+		// 	clientId: process.env.DISCORD_CLIENT_ID,
+		// 	clientSecret: process.env.DISCORD_CLIENT_SECRET,
+		// }),
+		// GitHubProvider({
+		// 	clientId: process.env.GITHUB_ID,
+		// 	clientSecret: process.env.GITHUB_SECRET,
+		// }),
 	],
 }
 
