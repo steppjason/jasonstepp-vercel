224f72e17a9cc203b7a40ac1ef14f975e0580b28
Jason Stepp <hi@jasonstepp.me>
2023-09-01 17:46:49 -0700

    Remove unused data

--- src/app/dashboard/_components/loginButton.tsx
+++ src/app/dashboard/_components/loginButton.tsx
@@ -3,9 +3,9 @@
 import { signIn, signOut, useSession } from 'next-auth/react'
 
 export default function LoginButton() {
-    const { status, data: session } = useSession()
+	const { data: session } = useSession()
 
-    if (session && session.user) return <button onClick={() => signOut()}>Sign Out</button>
+	if (session && session.user) return <button onClick={() => signOut()}>Sign Out</button>
 
-    return <button onClick={() => signIn()}>Sign In</button>
+	return <button onClick={() => signIn()}>Sign In</button>
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import { useSession } from 'next-auth/react'
 
 export default function Home() {
-	const { status, data: session } = useSession()
+	const { data: session } = useSession()
 
 	return <main>{session?.user?.name}</main>
 }
