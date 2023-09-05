daa87a2f0650511f13a3814e9fbf572cbbe58d0f
Jason Stepp <hi@jasonstepp.me>
2023-09-05 16:36:33 -0700

    Remove other auth comments

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,5 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
-// import FacebookProvider from 'next-auth/providers/facebook'
-// import DiscordProvider from 'next-auth/providers/discord'
-// import GitHubProvider from 'next-auth/providers/github'
 
 import type { NextAuthOptions } from 'next-auth'
 
@@ -12,18 +9,6 @@ const authOptions: NextAuthOptions = {
 			clientId: process.env.GOOGLE_CLIENT_ID,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 		}),
-		// FacebookProvider({
-		// 	clientId: process.env.FACEBOOK_CLIENT_ID,
-		// 	clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-		// }),
-		// DiscordProvider({
-		// 	clientId: process.env.DISCORD_CLIENT_ID,
-		// 	clientSecret: process.env.DISCORD_CLIENT_SECRET,
-		// }),
-		// GitHubProvider({
-		// 	clientId: process.env.GITHUB_ID,
-		// 	clientSecret: process.env.GITHUB_SECRET,
-		// }),
 	],
 }
 
